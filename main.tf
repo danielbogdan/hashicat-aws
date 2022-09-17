771555ff210bbff48665e26e3bd635f3324a5a5b
Daniel Bogdan <danielbogdan23@yahoo.com>
2022-09-17 17:05:57 +0000

    Added the first tag

--- main.tf
+++ main.tf
@@ -128,7 +128,8 @@ resource "aws_instance" "hashicat" {
 
   tags = {
     Name = "${var.prefix}-hashicat-instance"
-    Departments = "devops"
+    Departments = devops
+    Billable    = true
   }
 }
 
